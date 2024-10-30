04291f1402ee267280d051de55fd8eb09820ba44
Sichen (Gillian) Liu <sliuwizarding@gmail.com>
2024-10-30 01:44:13 -0400

    Partial update

--- src/main/java/edu/cmu/f24qa/loveletter/Deck.java
+++ src/main/java/edu/cmu/f24qa/loveletter/Deck.java
@@ -3,13 +3,25 @@ package edu.cmu.f24qa.loveletter;
 import java.util.Collections;
 import java.util.Stack;
 
-public class Deck {
-    private Stack<Card> deck;
+/**
+ * Represents a deck of cards used in the Love Letter game.
+ */
+public final class Deck {
+    /**
+     * The stack of cards representing the deck.
+     */
+    private final Stack<Card> deck;
 
+    /**
+     * Constructor for creating an empty deck.
+     */
     public Deck() {
         this.deck = new Stack<>();
     }
 
+    /**
+     * Builds the deck with a predefined set of cards.
+     */
     public void build() {
         for (int i = 0; i < 5; i++) {
             deck.push(Card.GUARD);
@@ -27,14 +39,27 @@ public class Deck {
         deck.push(Card.PRINCESS);
     }
 
+    /**
+     * Shuffles the deck.
+     */
     public void shuffle() {
         Collections.shuffle(deck);
     }
 
+    /**
+     * Draws a card from the deck.
+     *
+     * @return the card that was drawn from the deck
+     */
     public Card draw() {
         return deck.pop();
     }
 
+    /**
+     * Checks if there is any card left in the deck.
+     *
+     * @return true if there are cards remaining in the deck, false otherwise
+     */
     public boolean hasMoreCards() {
         return deck.size() > 0;
     }
