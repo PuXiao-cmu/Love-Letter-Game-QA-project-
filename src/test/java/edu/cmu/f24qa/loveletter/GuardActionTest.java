17789ed28e414eb07bd4af7225f81ce09ef6d1f1
Sichen (Gillian) Liu <sliuwizarding@gmail.com>
2024-11-06 18:47:27 -0500

    Create GuardActionTest.java
    This is used to test if Guard card allows user to select an opponent (if getOpponent() is called) when execute Guard actions.

---
+++ src/test/java/edu/cmu/f24qa/loveletter/GuardActionTest.java
@@ -0,0 +1,48 @@
+package edu.cmu.f24qa.loveletter;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the GuardAction class that focus on verifying that
+ * playing a Guard card prompts the selection of an opponent.
+ */
+class GuardActionTest {
+
+    private UserInput mockUserInput;
+    private Player mockUser;
+    private GuardAction guardAction;
+    private PlayerList mockPlayerList;
+    private Player mockOpponent;
+
+    /**
+     * Sets up the mock objects and the GuardAction instance before each test.
+     * Ensures that viewHandCard(0) on mockOpponent returns a valid card to avoid NullPointerExceptions.
+     */
+    @BeforeEach
+    void setUp() {
+        mockUserInput = mock(UserInput.class);
+        mockUser = mock(Player.class);
+        mockPlayerList = mock(PlayerList.class);
+        mockOpponent = mock(Player.class);
+
+        guardAction = new GuardAction();
+        
+        when(mockOpponent.viewHandCard(0)).thenReturn(Card.PRIEST);
+    }
+
+    /**
+     * Tests that playing a Guard card allows an opponent to be selected.
+     * Verifies that the getOpponent method is called on UserInput when GuardAction is executed.
+     */
+    @Test
+    void testPlayingGuardCardAllowsOpponentSelection() {
+        when(mockUserInput.getOpponent(mockPlayerList, mockUser)).thenReturn(mockOpponent);
+
+        guardAction.execute(mockUserInput, mockUser, mockPlayerList);
+
+        verify(mockUserInput).getOpponent(mockPlayerList, mockUser);
+    }
+}
