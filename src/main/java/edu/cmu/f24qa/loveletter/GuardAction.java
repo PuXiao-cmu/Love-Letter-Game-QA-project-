f2db707dcec1f15852001fba7bd6084014cc22d3
Sichen (Gillian) Liu <sliuwizarding@gmail.com>
2024-11-18 19:23:54 -0500

    Fix trailing spaces in GuardAction.java

--- src/main/java/edu/cmu/f24qa/loveletter/GuardAction.java
+++ src/main/java/edu/cmu/f24qa/loveletter/GuardAction.java
@@ -19,7 +19,6 @@ public class GuardAction implements CardAction {
         Player opponent = userInput.getOpponent(players, user);
 
         String cardName = userInput.getCardName();
-        
         Card opponentCard = opponent.viewHandCard(0);
         if (cardName.equalsIgnoreCase("Guard")) {
             System.out.println("You have guessed incorrectly");
