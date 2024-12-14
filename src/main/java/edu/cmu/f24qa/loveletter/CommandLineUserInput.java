874875d4a389f59f65cf727a69ec55956bc19148
Derek0000 <wendizhu@andrew.cmu.edu>
2024-12-14 00:19:20 +0000

    Fix getNumOpponent

--- src/main/java/edu/cmu/f24qa/loveletter/CommandLineUserInput.java
+++ src/main/java/edu/cmu/f24qa/loveletter/CommandLineUserInput.java
@@ -239,11 +239,11 @@ public class CommandLineUserInput implements UserInput {
             System.out.println("How many players would you like to select (please enter either 1 or 2): ");
             String number = scanner.nextLine();
 
-            if (!number.equals("1") || !number.equals("2")) {
+            if (number.equals("1") || number.equals("2")) {
+                return Integer.valueOf(number);
+            } else {
                 System.out.println("You can choose either 1 or 2 players. Please try again.");
             }
-
-            return Integer.valueOf(number);
         }
     }
 }
